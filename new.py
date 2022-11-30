144b5c6be9a0508f4f222d8a289d1d2ef50b8eb6
Shishir <shishirpandeya120@gmail.com>
2022-11-30 13:24:07 +0545

    The problem of how many guess left solved

--- new.py
+++ new.py
@@ -1,5 +1,6 @@
 n = 19
 i = 5
+print("You have altogether 5 guess")
 a = int(input("Guess one number and you will be guided thorougly: "))
 while(True):
     if a == n:
@@ -7,29 +8,29 @@ while(True):
         break
     while(i > 0):
         if a > 19 and a < 30:
-            print("Oops! Your number is slightly more...")
-            print("You have", i , "Guesses left")
-            a= int(input("Try Guessing Again: "))
             i= i - 1
+            print("Oops! Your number is slightly more...")
+            print("You have", i , "Guess left")
+            a= int(input("Try Guessing Again: "))
         if a < 19 and  a > 10:
-            print("Oops! Your number is slightly less...")
-            print("You have", i , "Guesses left")
-            a = int(input("Try Guessing Again: "))
             i = i - 1 
+            print("Oops! Your number is slightly less...")
+            print("You have", i , "Guess left")
+            a = int(input("Try Guessing Again: "))
         if a >= 30:
+            i = i - 1
             print("Your number is very large")
-            print("You have", i , "Guesses left")
+            print("You have", i , "Guess left")
             a = int(input("Try Guessing Again: "))
-            i = i - 1
         if a <= 10:
-            print("Your number is very Small")
-            print("You have", i , "Guesses left")
-            a = int(input("Try Guessing Again: "))
             i = i - 1
+            print("Your number is very Small")
+            print("You have", i , "Guess left")
+            a = int(input("Try Guessing Again: "))
         if a == n:
-            print("Congratulations! You have guessed it in ", 6-i ,"guesses")
+            print("Congratulations! You have guessed it in ", 6-i ,"guess")
             break
     break    
 if i == 0:
-    print("Oops You have lost all your guesses")            
+    print("Oops You have lost all your chances")            
 
