3b1bfc971bd9fdfe4ae1e95edd3adbaf055f42f6
Shishir <shishirpandeya120@gmail.com>
2022-12-01 12:52:33 +0545

    (y/n) problem remaining to solve

--- Guess_game.py
+++ Guess_game.py
@@ -1,27 +1,34 @@
-n = 19
-i = 5
-print("\n You have altogether 5 guess \n")
-a = int(input("Guess one number and you will be guided thorougly: "))
 while(True):
-    if a == n:
-        print("\n WOW! You did it in your first guess \n")
-        break
-    while(i > 0):
-        if a > 19:
-            i= i - 1
-            print("Oops! Your number is Greater")
-            print("You have", i , "Chance left \n")
-            a= int(input("Try Guessing Again: "))
-        if a < 19:
-            i = i - 1 
-            print("Oops! Your number is less")
-            print("You have", i , "Chance left \n")
-            a = int(input("Try Guessing Again: "))
+    while(True):
+        i = 5
+        n = 19
+        print("\n You have altogether 5 guess \n")
+        a = int(input("Guess one number and you will be guided thorougly: "))
+        # i = i - 1
         if a == n:
-            print("Congratulations! You have guessed it in ", 6-i ,"chance \n")
+            print("\n WOW! You did it in your first guess \n")
             break
-    break    
-if i == 0:
-    print("GAME OVER! You have lost all your chances")            
-
-
+        while(i > 0):
+            if a > 19:
+                print("Oops! Your number is High")
+                print("You have", i , "Chance left \n")
+                a= int(input("Try Guessing Again: "))
+                i = i - 1
+            if a < 19:
+                print("Oops! Your number is less")
+                print("You have", i , "Chance left \n")
+                a = int(input("Try Guessing Again: "))
+                i = i - 1 
+            if a == n:
+                print("Congratulations! You have guessed it in ", 6-i ,"chance ")
+                break
+        if i == 0:
+            print("GAME OVER! You have lost all your chances")       
+        Play_Again = input("\n Do you want to play again? (y/n) : ")
+        if Play_Again == "y":   
+            continue 
+        else:
+            print("\n Thank You! \n")
+            break
+        break
+    break
